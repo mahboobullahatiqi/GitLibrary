88b356521b8c356e8a0bf4ceb5788f9cc43303ac
mahboobullahatiqi <117614573+mahboobullahatiqi@users.noreply.github.com>
2022-11-09 12:36:49 -0800

    Update GitLaguna.java
    changes made to the file laguna

--- src/git/library/GitLaguna.java
+++ src/git/library/GitLaguna.java
@@ -5,5 +5,9 @@ public class GitLaguna {
 	public static void main(String[] args) {
 		System.out.println("git is ready");
 	}
+	
+	publich static void iAmIn () {
+		System.out.println("Code is transfered to eclips");
+	}
 
 }
